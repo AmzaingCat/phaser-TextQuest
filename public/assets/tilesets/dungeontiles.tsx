b1aaf640bd312f32134e07e3367fb8fed85700fb
Ifwat Amzar <ifwatamzar@gmail.com>
2025-05-28 14:55:23 +0800

    created a Tiled project to better edit tilemaps. tsx is TileSet XML

---
+++ public/assets/tilesets/dungeontiles.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dungeontiles" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="dungeon.png" width="192" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="entryX" type="int" value="0"/>
+   <property name="entryY" type="int" value="0"/>
+   <property name="targetScene" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="entryX" type="int" value="0"/>
+   <property name="entryY" type="int" value="0"/>
+   <property name="targetScene" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="entryX" type="int" value="0"/>
+   <property name="entryY" type="int" value="0"/>
+   <property name="targetScene" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collides" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="entryX" type="int" value="0"/>
+   <property name="entryY" type="int" value="0"/>
+   <property name="targetScene" value=""/>
+  </properties>
+ </tile>
+</tileset>
