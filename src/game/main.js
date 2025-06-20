7db75f7b1e86e1290bd4dfc70d8b61a547ac21d3
Ifwat Amzar <ifwatamzar@gmail.com>
2025-06-20 20:38:31 +0800

    make the game fit the browser no matter the size

--- src/game/main.js
+++ src/game/main.js
@@ -20,6 +20,10 @@ const config = {
     pixelArt: true,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'arcade'
     },
